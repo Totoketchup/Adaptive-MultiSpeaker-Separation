691c5479fe1367d5d25519e14660b2f91fd72ae7
Totoketchup <anthony.damato@hotmail.fr>
2017-07-18 15:22:03 +0200

    Create labels for the speaker indicies

--- data/data_tools.py
+++ data/data_tools.py
@@ -5,9 +5,7 @@ def read_data_header(subset_name=config.data_subset):
     subset = {}
     with open('data/LibriSpeech/SPEAKERS.TXT') as csvfile:
         spamreader = csv.reader(csvfile, delimiter='|')
-
         for row in spamreader:
-
             if row[0].startswith(';'): 
                 continue
             if row[2].strip() == subset_name:
@@ -20,9 +18,22 @@ def read_data_header(subset_name=config.data_subset):
         		continue
         	if row[3].strip() == subset_name:
         		subset[row[1].strip()]['chapters'].append(row[0].strip())
-    print subset
     return subset
 
 
+def males_keys(dico):
+    result = []
+    for key, value in dico.items():
+        if value['sex'] == 'M':
+            result += [key]
+    return result
 
+def females_keys(dico):
+    result = []
+    for key, value in dico.items():
+        if value['sex'] == 'F':
+            result += [key]
+    return result
 
+# dico =  read_data_header('dev-clean')
+# print females_keys(dico)
--- data/dataset.py
+++ data/dataset.py
@@ -5,122 +5,150 @@ import soundfile as sf
 import os
 import config
 from utils.audio import create_spectrogram
+from sets import Set
 
 class H5PY_RW:
-    def __init__(self):
-        self.current = None
+	def __init__(self):
+		self.current = None
 
-    def create_h5_dataset(self, output_fn, subset=config.data_subset, data_root=config.data_root):
-        speakers_info = data_tools.read_data_header(subset)
-        with h5py.File(output_fn,'w') as data_file:
-            for (key, elements) in speakers_info.items():
-                if key not in data_file:
-                    data_file.create_group(key)
-                print 'Speaker '+key
-                folder = data_root+'/'+subset+'/'+key # speaker folder
-                for chapter in elements['chapters']: # for all chapters read by this speaker
-                    print '-- Chapter '+chapter
-                    for root, dirs, files in os.walk(folder+'/'+chapter): # find all .flac audio
-                        for file in files:
-                            if file.endswith(".flac"):
-                                print '------ Track '+file
-                                path = os.path.join(root,file)
-                                raw_audio, samplerate = sf.read(path)
+	def create_h5_dataset(self, output_fn, subset=config.data_subset, data_root=config.data_root):
+		speakers_info = data_tools.read_data_header(subset)
+		with h5py.File(output_fn,'w') as data_file:
+			for (key, elements) in speakers_info.items():
+				if key not in data_file:
+					data_file.create_group(key)
+				print 'Speaker '+key
+				folder = data_root+'/'+subset+'/'+key # speaker folder
+				for chapter in elements['chapters']: # for all chapters read by this speaker
+					print '-- Chapter '+chapter
+					for root, dirs, files in os.walk(folder+'/'+chapter): # find all .flac audio
+						for file in files:
+							if file.endswith(".flac"):
+								print '------ Track '+file
+								path = os.path.join(root,file)
+								raw_audio, samplerate = sf.read(path)
 
-                                _, _, spec = create_spectrogram(raw_audio, samplerate)
+								_, _, spec = create_spectrogram(raw_audio, samplerate)
 
-                                data_file[key].create_dataset(file,
-                                    data=spec.T.astype(np.complex64),
-                                    compression="gzip",
-                                    dtype=np.complex64,
-                                    compression_opts=0)
-            
+								data_file[key].create_dataset(file,
+									data=spec.T.astype(np.complex64),
+									compression="gzip",
+									dtype=np.complex64,
+									compression_opts=0)
+			
 
-        print 'Dataset for the subset: ' + subset + ' has been built'
+		print 'Dataset for the subset: ' + subset + ' has been built'
 
-    def open_h5_dataset(self, filename):
-        self.h5 = h5py.File(filename, 'r')
-        self.keys = [key for key in self.h5]
-        items = []
-        for key in self.h5:
-            items += [key + '/' + val  for val in self.h5[key]]
-        self.raw_items = items
-        self.index_key = 0
-        self.index_item = 0
+	def open_h5_dataset(self, filename, subset=None):
+		self.h5 = h5py.File(filename, 'r')
+		
+		if subset == None:
+			self.keys = [key for key in self.h5]
+		else:
+			self.keys = subset
 
-    def set_chunk(self, chunk_size):
-        self.chunk_size = chunk_size
-        items = []
-        for item in self.raw_items:
-            L = self.h5[item].shape[0]//chunk_size
-            items += [item +'/' + str(part) for part in range(L)]
-        self.items = items
+		items = []
+		for key in self.keys:
+			items += [key + '/' + val  for val in self.h5[key]]
+		self.raw_items = items
+		self.index_key = 0
+		self.index_item = 0
 
-    def next(self):
-        item_path = self.items[self.index_item]
-        split = item_path.split('/')
-        key = int(split[0])
-        i = int(split[2])
-        item_path = '/'.join(split[:2])
-        X = self.h5[item_path][i*self.chunk_size : (i+1)*self.chunk_size]
+	def set_chunk(self, chunk_size):
+		self.chunk_size = chunk_size
+		items = []
+		for item in self.raw_items:
+			L = self.h5[item].shape[0]//chunk_size
+			items += [item +'/' + str(part) for part in range(L)]
+		self.items = items
 
-        self.index_item+=1
-        if self.index_item >= len(self.items):
-            self.index_item = 0
+	def next(self):
+		item_path = self.items[self.index_item]
+		split = item_path.split('/')
+		key = int(split[0])
+		i = int(split[2])
+		item_path = '/'.join(split[:2])
+		X = self.h5[item_path][i*self.chunk_size : (i+1)*self.chunk_size]
 
-        return X, key
+		self.index_item+=1
+		if self.index_item >= len(self.items):
+			self.index_item = 0
 
-    def shuffle(self):
-        permutation = np.random.shuffle(self.items)
+		return X, key
 
-    def speakers(self):
-        return self.keys
+	def shuffle(self):
+		permutation = np.random.shuffle(self.items)
 
-    def __iter__(self):
-        return self
+	def speakers(self):
+		return self.keys
+
+	def length(self):
+		return len(self.items)
+
+	def __iter__(self):
+		return self
 
 
 
 
 class Mixer:
-    def __init__(self, datasets, mixing_type='add'):
-        self.datasets = datasets
-        self.type = mixing_type       
+	def __init__(self, datasets, mixing_type='add'):
+		self.datasets = datasets
+		self.type = mixing_type
+		self.create_labels()
 
-    def shuffle(self):
-        for dataset in self.datasets:
-            dataset.shuffle()
+	def shuffle(self):
+		for dataset in self.datasets:
+			dataset.shuffle()
 
-    def next(self):
-        X_d = []
-        key_d = []
+	def next(self):
+		X_d = []
+		key_d = []
 
-        for dataset in self.datasets:
-            X, key = dataset.next()
-            X_d.append(X)
-            key_d.append(key)
+		for dataset in self.datasets:
+			X, key = dataset.next()
+			X_d.append(X)
+			key_d.append(key)
 
-        Y = np.argmax(np.array(X_d), axis=0)
-        Y[Y == 0] = -1
-        Y = np.array([Y , -Y])
-        Y = np.transpose(Y , (1,2,0))
+		X_d = np.array(X_d)
+		Y_pos = np.argmax(X_d, axis=0)
 
-        return X, Y, np.array(key_d)
+		Y = np.full((X_d.shape[1], X_d.shape[2], len(self.datasets)), -1)
 
-    def get_batch(self, batch_size):
-        X = []
-        Y = []
-        Ind = []
-        for i in range(batch_size):
-            x, y, ind = self.next()
-            X.append(x)
-            Y.append(y)
-            Ind.append(ind)
+		for i, row in enumerate(Y_pos):
+			for j, value in enumerate(row):
+				Y[i, j, value] = 1
 
-        return np.array(X), np.array(Y), np.array(Ind)
+		return X, Y, np.array(key_d)
 
-    def __iter__(self):
-        return self
+	def get_batch(self, batch_size):
+		X = []
+		Y = []
+		Ind = []
+		print self.dico
+		for i in range(batch_size):
+			x, y, ind = self.next()
+			X.append(x)
+			Y.append(y)
+			Ind.append([self.dico[j] for j in ind])
+
+		return np.array(X), np.array(Y), np.array(Ind)
+
+	def create_labels(self):
+		self.items = Set()
+		for dataset in self.datasets:
+			self.items.update(dataset.speakers())
+		self.items = list(self.items)
+		self.items.sort()
+		self.dico = {}
+		for i, item in enumerate(self.items):
+			self.dico[int(item)] = i
+
+	def __iter__(self):
+		return self
+
+	def get_labels(self):
+		return self.dico
 
 
 # H5 = H5PY_RW()
