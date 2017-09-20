1bfb87e2c9e19edc9579ca06002d2ae3d8b9b514
Anthony D'Amato <anthony.damato@hotmail.fr>
2017-09-20 16:27:07 +0200

    refact Adapt

--- models/adapt.py
+++ models/adapt.py
@@ -1,6 +1,6 @@
 # My Model 
 from utils.ops import ops
-from utils.ops.ops import Residual_Net, Conv1D, Reshape, Dense, unpool
+from utils.ops.ops import Residual_Net, Conv1D, Reshape, Dense, unpool, f_props
 from tensorflow.contrib.tensorboard.plugins import projector
 # from utils.postprocessing.reconstruction import 
 # import matplotlib.pyplot as plt
@@ -54,35 +54,35 @@ class Adapt:
 	## Front End creating STFT like data + P Matrix
 	##
 	@ops.scope
-	def front(self): 
+	def front(self):
 		# Front-End Adapt
 
 		# X_raw : [ B , T , 1]
 		X_in = tf.expand_dims(self.X_raw, 2)
 		self.input_shape = tf.shape(self.X_raw)
-        
-        # 1 Dimensional convolution along T axis with a window length = 1024
-        # And N filters
+		
+		# 1 Dimensional convolution along T axis with a window length = 1024
+		# And N filters
 		self.WC1 = tf.Variable(tf.random_normal([1024, 1, self.N], stddev=0.35))
 		X = tf.nn.conv1d(X_in, self.WC1, stride=1, padding="SAME") 
 
 		# X : [ B , T , N]
 		X_abs = tf.abs(X)
-        
-        # Smoothing the 'STFT' like created by the previous OP
+		
+		# Smoothing the 'STFT' like created by the previous OP
 		smoothing_filter = tf.Variable(tf.random_normal([1,4,1,1], stddev=0.35))
 		M = tf.nn.conv2d(tf.expand_dims(X_abs,3), smoothing_filter, strides=[1, 1, 1, 1], padding="SAME")
-     	
+		
 		M = tf.nn.softplus(M)
 
 		# Matrix for the reconstruction process P = M / X => X * P = M
 		self.P = M / tf.expand_dims(X,3)
-        
-        # Max Pooling
+		
+		# Max Pooling
+		#with tf.device('/GPU'):
 		y, self.argmax = tf.nn.max_pool_with_argmax(M, (1, self.max_pool_value, 1, 1), strides=[1, self.max_pool_value, 1, 1], padding="SAME")
-        
-		return y
 
+		return y
 	@ops.scope
 	def separator(self):
 		## ##
@@ -95,11 +95,6 @@ class Adapt:
 			Dense(500, 500, tf.nn.softplus),
 			Dense(500, self.N, tf.nn.softplus)
 		]
-
-		def f_props(layers, x):
-			for i, layer in enumerate(layers):
-				x = layer.f_prop(x)
-			return x
 
 		separator_out = f_props(layers, tf.reshape(input, [-1,self.N]))
 		return tf.expand_dims(tf.reshape(separator_out, shape),3)
@@ -121,7 +116,7 @@ class Adapt:
 	@ops.scope
 	def cost(self):
 		# Definition of cost for Adapt model
-		reg = 0.01*tf.norm(self.X_raw, axis=1)
+		reg = 0.001*tf.norm(self.X_raw, axis=1)
 		cost = tf.reduce_sum(tf.pow(self.X_raw - self.back, 2), axis=1) + reg
 		cost = tf.reduce_mean(cost)
 		tf.summary.scalar('training cost', cost)
@@ -138,11 +133,11 @@ class Adapt:
 		summary, _, cost = self.sess.run([self.merged, self.optimize, self.cost], {self.X_raw: X})
 		self.train_writer.add_summary(summary, step)
 		return cost
-    
+	
 	def test(self, X):
 		cost = self.sess.run(self.back, {self.X_raw: X})
 		return cost
-    
+	
 
 
 if __name__ == "__main__":
@@ -158,16 +153,16 @@ if __name__ == "__main__":
 	print data.shape
 	ada = Adapt()
 	ada.init()
-    
+	
 	for u in range(1):
 		for i in range(sub/batch_size):
 			y = ada.train(data[i:(i+1)*batch_size, :], (sub/batch_size)*u+i)
 			print y
-    
+	
 	X_reconstruct = ada.test(data)
 	sf.write('test_recons.flac', np.flatten(X_reconstruct), fs)
 
-        
+		
 	# y = np.reshape(y, (195, 256))
 	print y.shape
 	print np.count_nonzero(y)
