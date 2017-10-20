793ea667e53b477e0e2331c03c593334c55eb38a
Anthony D'Amato <anthony.damato@hotmail.fr>
2017-10-20 12:17:18 +0200

    add inertia criteria to Kmeans

--- models/Kmeans_2.py
+++ models/Kmeans_2.py
@@ -14,10 +14,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 class KMeans:
 
-	def __init__(self, nb_clusters, centroids_init=None, nb_iterations=50, graph=None, input_tensor=None):
+	def __init__(self, nb_clusters, centroids_init=None, nb_tries=10, nb_iterations=10, graph=None, input_tensor=None):
 
 		self.nb_clusters = nb_clusters
 		self.nb_iterations = nb_iterations
+		self.nb_tries = nb_tries
 
 		if input_tensor == None:
 			self.graph = tf.Graph()
@@ -28,9 +29,12 @@ class KMeans:
 			if input_tensor == None:
 				# Spectrogram, embeddings
 				# shape = [batch, L , E ]
-				self.X = tf.placeholder("float", [None, None, None])
+				self.X_in = tf.placeholder("float", [None, None, None])
+				self.b = tf.shape(self.X_in)[0]
 			else:
 				self.X = input_tensor
+
+			self.X = tf.tile(self.X_in, [self.nb_tries, 1, 1])
 
 			self.B = tf.shape(self.X)[0]
 			self.L = tf.shape(self.X)[1]
@@ -65,7 +69,19 @@ class KMeans:
 		cond = lambda i, m: tf.less(i, self.nb_iterations)
 		_ , self.centroids = tf.while_loop(cond, self.body,[i, self.centroids], shape_invariants=[i.get_shape(), tf.TensorShape([None, None, None])])
 
-		return self.centroids, self.get_labels(self.centroids, self.X)
+		centroids = tf.expand_dims(self.centroids, 1)
+		X = tf.expand_dims(self.X, 2)
+		inertia = tf.reduce_sum(tf.norm(X - centroids, axis=3), axis=[1,2])
+
+		inertia = tf.reshape(inertia, [self.b, self.nb_tries])
+		bests = tf.argmin(inertia, 1, output_type=tf.int32)
+		index = bests + tf.range(self.b)*self.nb_tries
+
+		self.centroids = tf.reshape(self.centroids, [self.b*self.nb_tries, self.nb_clusters, self.E])
+
+		self.centroids = tf.gather(self.centroids, index)
+		print self.centroids
+		return self.centroids, self.get_labels(self.centroids, self.X_in)
 
 
 	def body(self ,i, centroids):
@@ -89,24 +105,35 @@ class KMeans:
 		X_ = tf.expand_dims(X, 2)
 		return tf.argmin(tf.norm(X_ - centroids_, axis=3), axis=2, output_type=tf.int32)
 
-
 	def fit(self, X_train):
-		return self.sess.run(self.network, {self.X: X_train})
+		return self.sess.run(self.network, {self.X_in: X_train})
+
+from sklearn.cluster import KMeans as km
 
 if __name__ == "__main__":
-	nb_samples = 10000
-	E = 2
-	nb_clusters = 2
+	nb_samples = 100
+	E = 50
+	nb_clusters = 5
+	error = 0
+	TOTAL = 100
+	nb_err = 0.0
+	kmean = KMeans(nb_clusters, nb_tries=1, nb_iterations=20)
 	
-	X, y = make_blobs(n_samples=nb_samples, centers=nb_clusters, n_features=E)
-	X = X[np.newaxis,:]
-	y = y[np.newaxis,:]
-	print y
-	kmean = KMeans(nb_clusters)
-	kmean.init()
-	centroids, labels = kmean.fit(X)
-	print labels
-	print y
-	if np.all((y-labels) == 0) or np.all((y+labels) == 1):
-		print 'OK'
 
+	for i in range(TOTAL):	
+		X, y = make_blobs(n_samples=nb_samples, centers=nb_clusters, n_features=E, cluster_std=2.0)
+		X_ = X[np.newaxis,:]
+		y = y[np.newaxis,:]
+		
+		kmean.init()
+		centroids, labels = kmean.fit(X_)
+		centroids = np.reshape(centroids, (nb_clusters, E))
+		kmeans = km(n_clusters=nb_clusters, random_state=0, ).fit(X)
+		error = np.sum(np.square(np.sort(centroids,0) - np.sort(kmeans.cluster_centers_,0)))
+		if error > 0.1:
+			print error
+			nb_err += 1.0
+
+	print nb_err / float(TOTAL)
+
+	print error
