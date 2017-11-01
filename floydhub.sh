29dc80e007a385ab3276f478314d430762abdb3c
Anthony D'Amato <anthony.damato@hotmail.fr>
2017-11-01 16:08:32 +0100

    update bash for floydhub jobs

--- floydhub.sh
+++ floydhub.sh
@@ -1,3 +1,3 @@
 floyd init das
 
-floyd run --data totoketchup/datasets/audio_norm_raw_16k/2:/h5py_files --env tensorflow-1.3:py2 --tensorboard --gpu --mode jupyter
+floyd run --data totoketchup/datasets/audio_norm_raw_16k/2:/h5py_files --env tensorflow-1.3:py2 --tensorboard --gpu "pip install --upgrade tensorflow-gpu==1.4rc1 && python -m experiments.pretraining"
---
+++ floydhub_dpcl_finetuning.sh
@@ -0,0 +1,8 @@
+floyd init das
+
+floyd run \
+--data totoketchup/datasets/audio_norm_raw_16k/2:/h5py_files \
+--data totoketchup/projects/das/76/output:/model \
+--env tensorflow-1.3:py2 \
+--tensorboard --gpu \
+"pip install --upgrade tensorflow-gpu==1.4rc1 && python -m experiments.dpcl_finetuning.py"
