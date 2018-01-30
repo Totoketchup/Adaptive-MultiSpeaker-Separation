a3affe6380f5a8e463fd33f2908e264e44646fa0
Anthony D'Amato <anthony.damato@hotmail.fr>
2018-01-30 11:02:40 +0900

    modif script

--- job.sh
+++ job.sh
@@ -12,10 +12,27 @@ export FTP_PROXY=$MY_PROXY_URL
 export http_proxy=$MY_PROXY_URL 
 export https_proxy=$MY_PROXY_URL
 export ftp_proxy=$MY_PROXY_URL
-export PATH="/home/anthony/anaconda2/bin:$PATH"
-export LD_LIBRARY_PATH="/home/anthony/anaconda2/lib:$LD_LIBRARY_PATH"
+
 
 . /fefs/opt/dgx/env_set/common_env_set.sh
+#. /fefs/opt/dgx/env_set/nvcr-tensorflow-1712.sh
 
-pip install --user -r requirements.txt
+export PATH="/home/anthony/anaconda2/bin:$PATH"
+export LD_LIBRARY_PATH="/home/anthony/anaconda2/lib:$LD_LIBRARY_PATH"
+export PATH="/home/anthony/anaconda2/lib:$PATH"
+export LIBRARY_PATH="/home/anthony/anaconda2/lib:$LIBRARY_PATH"
+export PATH="/home/anthony/.local/bin:$PATH"
+
+
+
+#printenv
+#pip install --user -r requirements.txt
+#pip install tensorflow-gpu
+#nvidia-smi
 python -m experiments.pretraining
+#python test_tf.py
+#pip uninstall tensorflow
+#pip uninstall tensorflow-gpu
+#pip install --user tensorflow-gpu
+#conda remove tensorflow
+#conda install tensorflow-gpu
