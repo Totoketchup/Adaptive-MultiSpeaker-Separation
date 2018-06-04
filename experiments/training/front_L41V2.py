51198a285e23d49eefd06ae5ad59119124640596
Anthony D'Amato <anthony.damato@hotmail.fr>
2018-06-04 15:43:11 +0900

    add front + SCE + DANet

---
+++ experiments/training/front_L41V2.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+from utils.trainer import MyArgs, Front_Separator_Trainer
+from models.SC_V2 import L41ModelV2
+
+if __name__ == '__main__':
+	p = MyArgs()
+
+	# Adapt model to load + params
+	p.parser.add_argument(
+		'--model_folder', help='Path to Adapt folder to load', required=True)
+
+	p.add_separator_args()
+	args = p.get_args()
+
+	trainer = Front_Separator_Trainer(L41ModelV2, 'front_DANet_SCE', pretraining=False, **vars(args))
+	trainer.train()
